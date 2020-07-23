e65d1e992fcfcd6591edd8d10e853b95a7db8d86
huyuanixn325 <huyuanxin0325@163.com>
2020-07-23 21:33:40 +0800

    完成should_return_32_when_given_normal_user_pos_amont_25_18_108_10_22_208

--- src/test/java/com/thoughtworks/basic/CreditCardIntegralTest.java
+++ src/test/java/com/thoughtworks/basic/CreditCardIntegralTest.java
@@ -109,4 +109,35 @@ public class CreditCardIntegralTest {
         //then
         Assert.assertEquals(expectresult,result);
     }
+
+    @Test
+    public void should_return_32_when_given_normal_user_pos_amont_25_18_108_10_22_208(){
+        //given
+        Consume consume = new Consume("normal","2020-07-01 12:20","微信支付消费",new BigDecimal(25));
+        Consume consumeA = new Consume("normal","2020-07-01 12:50","微信支付消费",new BigDecimal(18));
+        Consume consumeB = new Consume("normal","2020-07-01 18:50","POS机消费",new BigDecimal(108));
+        Consume consumeC = new Consume("normal","2020-07-02 08:20","微信支付消费",new BigDecimal(10));
+        Consume consumeD = new Consume("normal","2020-07-02 12:20","微信支付消费",new BigDecimal(22));
+        Consume consumeE = new Consume("normal","2020-07-02 18:50","POS机消费",new BigDecimal(208));
+
+        List<Consume> consumes = new ArrayList<>();
+        consumes.add(consume);
+        consumes.add(consumeA);
+        consumes.add(consumeB);
+        consumes.add(consumeC);
+        consumes.add(consumeD);
+        consumes.add(consumeE);
+        CreditCardIntegral creditCardIntegral = new CreditCardIntegral(consumes);
+        String expectresult = "总积分：32\n" +
+                "2020-07-01 12:20 微信支付消费 25元，积分 +1\n"+
+                "2020-07-01 12:50 微信支付消费 18元，积分 +0\n"+
+                "2020-07-01 18:50 POS机消费 108元，积分 +10\n"+
+                "2020-07-02 08:20 微信支付消费 10元，积分 +0\n"+
+                "2020-07-02 12:20 微信支付消费 22元，积分 +1\n"+
+                "2020-07-02 18:50 POS机消费 208元，积分 +20"  ;
+        //when
+        String  result=creditCardIntegral.countIntegral();
+        //then
+        Assert.assertEquals(expectresult,result);
+    }
 }
